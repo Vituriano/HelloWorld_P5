9f7b51e07e70c94016e873ff851882b8cd4e0ec4
vituriano <viturianooliveira@gmail.com>
2019-10-23 21:04:36 -0400

    adicionei mais paredes

--- sketch.js
+++ sketch.js
@@ -16,15 +16,39 @@ class Ball {
   update() {
     this.position = vectorSum(this.position, this.velocity);
     this.velocity = vectorSum(this.velocity, gravity);
-    if (this.checkCollision()) {
+    if (this.checkCollision() == 1) {
       // a bolinha colidiu
       this.velocity[1] = ceil(-this.velocity[1]);
-      this.position[1] = height - this.radius;
+      this.position[1] = height - this.radius - 1;
+    }
+    if (this.checkCollision() == 2) {
+      // a bolinha colidiu
+      this.velocity[1] = ceil(-this.velocity[1]);
+      this.position[1] = this.radius + 1;
+    }
+    if (this.checkCollision() == 3) {
+      // a bolinha colidiu
+      this.velocity[0] = ceil(-this.velocity[0]);
+      this.position[0] = width - this.radius - 1;
+    }
+    if (this.checkCollision() == 4) {
+      // a bolinha colidiu
+      this.velocity[0] = ceil(-this.velocity[0]);
+      this.position[0] = this.radius + 1;
     }
   }
   checkCollision() {
     if (this.radius + this.position[1] >= height) {
-      return true;
+      return 1;
+    }
+    if (this.position[1] - this.radius <= 0) {
+      return 2;
+    }
+    if (this.radius + this.position[0] >= width) {
+      return 3;
+    }
+    if (this.position[0] - this.radius <= 0) {
+      return 4;
     }
     return false;
   }
